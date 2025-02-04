efb5352aa1bc3f4887470d652a1e6ad21f912164
insane_arnold <limbunihal07@gmail.com>
2025-02-05 01:37:25 +0530

    added Navbar from tailwind components

--- FrontEndDev/e-commerce/src/App.js
+++ FrontEndDev/e-commerce/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-      Hello Ecommerce
+      <Navigation/>
+      <div>
+        Home
+      </div>
     </div>
   );
 }
